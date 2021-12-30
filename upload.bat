15704cac192422db7c0f34e5be404f35703fbf33
DybaTube <joaodyba@gmail.com>
2021-12-30 20:27:36 -0300

    Initial commit

--- upload.bat
+++ upload.bat
@@ -1,14 +1,21 @@
 @echo off
 title Step 1/3 - adding to the commit
 echo ADDING EVERYTHING TO THE COMMIT
-echo PRESS ENTER to continue
+echo PRESS any key to continue
 pause >nul
 git add --all
 cls
 echo FINISHED ADDING
-echo press ENTER to continue
+echo press any key to continue
 title Step 1/3 - finished adding to the commit
 pause >nul
+cls
+title Step 1/2 - add the commit
+echo ADDING ALL THE COMMIT TO THE GITHUB
+echo press any key to continue
+pause >nul
 git commit -m "Initial commit"
-
+title Step 1/2 - finished adding the commit
+echo FINISHED ADDING THE COMMIT TO THE GITHUB
+title Last step - Upload to the github server
 git push -u origin main
