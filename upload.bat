aefe53dd874354745c9d92efd4b833b2a286de5a
DybaTube <joaodyba@gmail.com>
2021-12-30 20:24:54 -0300

    Initial commit

--- upload.bat
+++ upload.bat
@@ -1,8 +1,12 @@
 @echo off
-git add --all
-title "Step 1/3 - adding to the commit"
-echo "ADDING EVERYTHING TO THE COMMIT"
+title Step 1/3 - adding to the commit
+echo ADDING EVERYTHING TO THE COMMIT
 echo PRESS ENTER to continue
+pause >nul
+git add --all
+cls
+echo FINISHED ADDING
+echo press ENTER to continue
 pause >nul
 git commit -m "Initial commit"
 
